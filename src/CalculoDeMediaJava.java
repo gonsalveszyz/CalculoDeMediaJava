06df21010409eff91d6e945fc3755b8bfe876ecb
Arthur Gonçalves de Oliveira <oliveraarthurrtx@gmail.com>
2026-04-14 15:03:26 -0300

    media calculator

---
+++ src/CalculoDeMediaJava.java
@@ -0,0 +1,7 @@
+public class CalculoDeMediaJava{
+    static void main(String[] args) {
+        double nota1 =9.0;
+                double  nota2 =7.0;
+        System.out.println(nota1+nota2/2);
+    }
+}
